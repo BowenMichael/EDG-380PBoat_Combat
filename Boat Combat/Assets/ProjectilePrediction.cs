1b7b50f5d9aba5aa85998d4cd09d25ad2f858ce0
Michael Bowen <michael@thebowenfamily.com>
2022-02-12 13:28:16 -0500

    Turned offline off indicator ismine check

--- Boat Combat/Assets/ProjectilePrediction.cs
+++ Boat Combat/Assets/ProjectilePrediction.cs
@@ -7,8 +7,6 @@ namespace Com.BowenIvanov.BoatCombat {
     {
         public float timeIterations = 10;
         public GameObject indicator;
-        public float distancePerIteration = 1;
-        public float gravity = 9.8f;
 
         Transform parent;
         private PlayerManager plrM;
@@ -18,13 +16,19 @@ namespace Com.BowenIvanov.BoatCombat {
         {
             plrM = GetComponent<PlayerManager>();
             //parent = Instantiate(new GameObject()).transform;
-            aim = Instantiate(indicator);
+            if (plrM.photonView.isMine)
+            {
+                aim = Instantiate(indicator);
+            }
         }
 
         // Update is called once per frame
         void Update()
         {
-            waterTarget();
+            if (plrM.photonView.isMine)
+            {
+                waterTarget();
+            }
             //projectArc();
         }
 
@@ -36,7 +40,7 @@ namespace Com.BowenIvanov.BoatCombat {
             Vector3 projectileDirection = new Vector3(-cameraDirection.x, cameraDirection.y, -cameraDirection.z).normalized;
             float speed = ((plrM.getProjSpeed())/plrM.testProjectile.GetComponent<Rigidbody>().mass) * Time.fixedDeltaTime;
             Vector3 v = speed * projectileDirection * Time.fixedDeltaTime;
-            Debug.Log(v);
+            //Debug.Log(v);
             for (int t = 0; t < 1000; t++)
             {
                 float time = t * Time.fixedDeltaTime;
@@ -44,7 +48,7 @@ namespace Com.BowenIvanov.BoatCombat {
                 //Debug.Log(yPos);
                 if (yPos < 0)//xPos < ground plane
                 {
-                    Debug.Log("Set Aim pos" + t);
+                    //Debug.Log("Set Aim pos" + t);
                     float xPos = pos.x + v.x * time;
                     float zPos = pos.z + v.z * time;
                     aim.transform.position = new Vector3(xPos, 1.0f, zPos);
@@ -54,25 +58,7 @@ namespace Com.BowenIvanov.BoatCombat {
             }
         }
 
-        void projectArc()
-        {
-            Destroy(parent.gameObject);
-            parent = null;
-            parent = Instantiate(indicator).transform;
-            Vector3 pos = gameObject.transform.position;
-            pos += new Vector3(0.0f, 1.0f, 0.0f);
-            Vector3 cameraDirection = (Camera.main.transform.position - gameObject.transform.position).normalized;
-            Vector3 projectileDirection = new Vector3(-cameraDirection.x, cameraDirection.y, -cameraDirection.z).normalized;
-            for (int i = 0; i < timeIterations; i++)
-            {
-                Debug.DrawRay(pos, projectileDirection);
-                if (i >= 6)
-                    Instantiate(indicator, pos, Quaternion.identity, parent);
-                projectileDirection = new Vector3(projectileDirection.x, projectileDirection.y - (((i / timeIterations)) * gravity), projectileDirection.z) * distancePerIteration;
-                pos += projectileDirection;
-
-            }
-        }
+       
 
         
     }
